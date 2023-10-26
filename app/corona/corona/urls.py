0052fd60ad1b3efd92fb52e56042bde7e64a333b
Alex45682 <alexandraalexeenko647@gmail.com>
2023-10-26 16:57:13 +0300

    add email service

--- app/corona/corona/urls.py
+++ app/corona/corona/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 
 
 from companies.urls import companies_router
+from companies.views import send_company_email
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(companies_router.urls)),
+    path("send_email", send_company_email),
 ]
